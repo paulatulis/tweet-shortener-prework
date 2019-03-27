3938502b2fbfb7541af99d3bb16c1b7290fe4f68
Paula Tulis <tulis.paula@gmail.com>
2019-03-27 01:30:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -16,18 +16,14 @@ end
 #tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
 
  def word_substituter(tweet)
-   shorter_words = dictionary
-   words = tweet.split(" ")
-   
-   words.collect do |word|
-     dictionary.collect do |key, value|
-       if word == key 
-         word.replace(value)
-       end
-     end 
-   end 
-   words.join(" ")
- end 
+ dictionary.keys = sub_words
+ new_tweet = []
+ tweet_array = tweet.split(" ")
+ tweet_array.collect do |word|
+ sub_words.include?(word) ? new_tweet << dictionary[word] : new_tweet << word
+ end
+ new_tweet.join(" ")
+end
  
  def bulk_tweet_shortener(tweet)
   tweet.map do |thing|
